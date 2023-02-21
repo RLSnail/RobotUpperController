1940f84b9570982cc1450f94be8f1a65c2bc333e
RLSnail <13355365693@qq.com>
2023-02-21 16:52:27 +0800

    删除了参数字典

--- RobotController.cs
+++ RobotController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace RobotUpprtController
 {
@@ -12,29 +13,21 @@ namespace RobotUpprtController
     {
         //定义一个串口控制器
         SerialController serialController;
-        //定义一个参数字典，保存下位机发来的数据
-        Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+        //定义一个定时器定时更新数据
+        System.Timers.Timer timer_update = null;
 
         public RobotController(string portName, int baudrate)
         {
             //初始化串口控制器
             serialController = new SerialController(portName, baudrate);
             serialController.DataReceived += new DataReceivedHandler(OnDataReceived);
+
+            //初始化定时器
         }
 
         void OnDataReceived(string name, string[] values)
         {
-            //串口控制器收到了下位机传来的格式化数据，每次收到数据都要更新参数字典
-            //先判断有没有这个属性
-            if (parameters.ContainsKey(name))
-            {
-                parameters[name] = values;
-            }
-            else
-            {
-                //如果属性不存在，那就添加进去
-                parameters.Add(name, values);
-            }
+            //串口控制器收到了下位机传来的格式化数据
         }
     }
 }
