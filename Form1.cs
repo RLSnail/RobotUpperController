4630baf3abf2ba71b5de12c30140dd61724342a1
RLSnail <13355365693@qq.com>
2023-02-21 18:11:33 +0800

    1

--- Form1.cs
+++ Form1.cs
@@ -12,15 +12,18 @@ namespace RobotUpprtController
 {
     public partial class Form1 : Form
     {
-        RobotController rc = new RobotController("COM3", 115200);
+        RobotController rc = null;
         public Form1()
         {
             InitializeComponent();
+
+            //初始化机器人控制器
+            rc = new RobotController("COM3", 115200);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
